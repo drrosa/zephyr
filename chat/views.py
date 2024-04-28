0120f59b3bf237b62fa2fa3c7b0278bc6a762853
David Rosa <rosatamsen@gmail.com>
2024-04-28 15:39:16 -0400

    auth: Add login functionality.

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect("home")
