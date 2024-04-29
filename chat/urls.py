3d36efe6656e4d4d5c52953e0c8d0cd178448762
David Rosa <rosatamsen@gmail.com>
2024-04-28 21:07:09 -0400

    user: Implement signup functionality.

--- chat/urls.py
+++ chat/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("accounts/logout/", views.logout_view, name="logout"),
+    path("accounts/signup/", views.signup, name="signup"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
 
 def home(request):
@@ -11,3 +12,18 @@ def home(request):
 def logout_view(request):
     logout(request)
     return redirect("home")
+
+
+def signup(request):
+    error_message = ""
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+        else:
+            error_message = "Invalid signup. Try again!"
+    form = UserCreationForm()
+    context = {"form": form, "error_message": error_message}
+    return render(request, "registration/signup.html", context)
