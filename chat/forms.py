7f19da14bf3381f19be211d42b3ae79cef6b0f3e
David Rosa <rosatamsen@gmail.com>
2024-04-30 19:45:20 -0400

    messages: Implement create functionality.

---
+++ chat/forms.py
@@ -0,0 +1,20 @@
+from django.forms import ModelForm
+from django import forms
+from .models import Message
+
+
+class MessageForm(ModelForm):
+    class Meta:
+        model = Message
+        fields = ("content",)
+        widgets = {
+            "content": forms.TextInput(
+                attrs={
+                    "name": "message",
+                    "placeholder": "Add message ...",
+                    "class": "w-full h-20 p-4 rounded-xl",
+                    "autofocus": True,
+                    "autocomplete": "off",
+                }
+            )
+        }
--- chat/views.py
+++ chat/views.py
@@ -3,13 +3,25 @@ from django.contrib.auth import logout, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from .models import Conversation
+from .forms import MessageForm
 
 
 @login_required
 def home(request):
     welcome_topic = get_object_or_404(Conversation, topic_name="welcome")
     chat_messages = welcome_topic.chat_messages.all()[:30]
-    return render(request, "home.html", {"chat_messages": chat_messages})
+    form = MessageForm()
+
+    if request.method == "POST":
+        form = MessageForm(request.POST)
+        if form.is_valid:
+            message = form.save(commit=False)
+            message.sender = request.user
+            message.thread = welcome_topic
+            message.save()
+            return redirect("home")
+
+    return render(request, "home.html", {"chat_messages": chat_messages, "form": form})
 
 
 @login_required
